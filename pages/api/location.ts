ddefe2e0405592b9efa8c22c25b919ff2f47a5ec
Edward Thomson <ethomson@vercel.com>
2023-01-26 20:20:10 +0000

    where...am i?

---
+++ pages/api/location.ts
@@ -0,0 +1,45 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export const config = {
+  runtime: 'edge'
+};
+
+export type Location = {
+  ip: string,
+  city: string,
+  region: string | null,
+  country: string,
+  latitude: string,
+  longitude: string,
+  timezone: string
+};
+
+const devLocation: Location = {
+  'ip': '127.0.0.1',
+  'city': 'Brescia',
+  'region': 'Lombardy',
+  'country': 'IT',
+  'latitude': '45.5336',
+  'longitude': '10.2194',
+  'timezone': 'Europe/Rome'
+};
+
+export default function handler(req: NextRequest, res: NextResponse) {
+  let data: Location;
+
+  if (process.env.NODE_ENV === "development") {
+    data = devLocation;
+  } else {
+    data = {
+      ip: req.headers.get('x-real-ip') || '127.0.0.1',
+      city: req.headers.get('x-vercel-ip-city') || 'Unknown',
+      region: req.headers.get('x-vercel-ip-country-region'),
+      country: req.headers.get('x-vercel-ip-country') || '??',
+      latitude: req.headers.get('x-vercel-ip-latitude') || '0.0000',
+      longitude: req.headers.get('x-vercel-ip-longitude') || '0.0000',
+      timezone: req.headers.get('x-vercel-ip-timezone') || 'Unknown'
+    }
+  };
+
+  return NextResponse.json(data);
+}
